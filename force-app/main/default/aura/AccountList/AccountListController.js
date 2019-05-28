1fb8957ec08d59ccd933efce9f6199472c733b90
Amit Kumar <a.kumar@globant.com>
2019-05-28 16:31:01 +0530

    add contact assignment

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -1,44 +1,69 @@
 ({
-      doInit: function(component, event, helper) {
+    doInit: function(component, event, helper) {
         // Fetch the account list from the Apex controller
         helper.getAccountList(component);
-      },
-        
-      fetchContacts: function(component, event, helper) {
+    },
+    fetchContacts: function(component, event, helper) {
         // Prevent the form from getting submitted
         event.preventDefault();
+        
         // Get the value from the field that's in the form
         var accountId = event.target.getElementsByClassName('account-id')[0].value;
         component.set('v.accountId', accountId);
+        
         helper.showContactList(component, accountId);
-      },
-        createContacts: function(component, event, helper) {
-        event.preventDefault();
-        helper.showPopupHelper(component, 'modaldialog', 'slds-fade-in-');
-		helper.showPopupHelper(component,'backdrop','slds-backdrop--');
-      },
-        closePopUp : function(component, event, helper) {
-            console.log('here');
-            event.preventDefault();
-        helper.hidePopupHelper(component, 'modaldialog', 'slds-fade-in-');
-		helper.hidePopupHelper(component,'backdrop','slds-backdrop--');
-            
     },
-        handleSuccess : function(component, event, helper) {
-              event.preventDefault();
-            helper.hidePopupHelper(component, 'modaldialog', 'slds-fade-in-');
-		helper.hidePopupHelper(component,'backdrop','slds-backdrop--');
-             helper.showContactList(component, component.get('v.accountId'));
-        },
-        createRecord : function (component, event, helper) {
-    var createRecordEvent = $A.get("e.force:createRecord");
-    createRecordEvent.setParams({
-        "entityApiName": "Contact"
-    });
-    createRecordEvent.fire();
-}
+    createContacts: function(component, event, helper) {
+        // Prevent the form from getting submitted
+        event.preventDefault();
         
+        helper.showPopupHelper(component, 'modaldialog', 'slds-fade-in-');
+        helper.showPopupHelper(component,'backdrop','slds-backdrop--');
         
+        var accountId = event.target.getElementsByClassName('account-id')[0].value;
+        component.set('v.accountId', accountId);
         
+    },
+    closePopUp : function(component, event, helper) {
+        event.preventDefault();
         
-    })
+        helper.hidePopupHelper(component, 'modaldialog', 'slds-fade-in-');
+        helper.hidePopupHelper(component,'backdrop','slds-backdrop--');
+        
+        helper.showContactList(component, component.get('v.accountId'));
+        
+    },
+    handleSuccess : function(component, event, helper) {
+        event.preventDefault();
+        
+        helper.hidePopupHelper(component, 'modaldialog', 'slds-fade-in-');
+        helper.hidePopupHelper(component,'backdrop','slds-backdrop--');
+        
+        helper.showContactList(component, component.get('v.accountId'));
+    },
+    createNewContact : function(component, event, helper) {
+        var validStudent = component.find('contactform').reduce(function (validSoFar, inputCmp) {
+            
+            // Displays error messages for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        
+        // If we pass error checking, do some real work
+       	if(validStudent){
+            // Create the new Contact
+            helper.createContactForAccount(component)  
+       	}
+    },
+    
+     showLess: function(component, componentId, className){
+		 var accounts = component.get('v.lessAccounts');
+         component.set('v.accounts',accounts);
+      },
+     showMore: function(component, componentId, className){
+         var accounts = component.get('v.allAccounts');
+         component.set('v.accounts',accounts);
+      },
+
+    
+})
--- force-app/main/default/aura/AccountList/AccountListHelper.js
+++ force-app/main/default/aura/AccountList/AccountListHelper.js
@@ -2,36 +2,66 @@
       // Fetch the accounts from the Apex controller
       getAccountList: function(component) {
         var action = component.get('c.getAccounts');
+          
         // Set up the callback
         var self = this;
         action.setCallback(this, function(actionResult) {
-         component.set('v.accounts', actionResult.getReturnValue());
+            var accounts = actionResult.getReturnValue();
+            var lessAccounts = accounts.slice(0,10)
+         	component.set('v.accounts',lessAccounts);
+            component.set('v.lessAccounts',lessAccounts);
+            component.set('v.allAccounts',accounts);
+            
         });
+          
         $A.enqueueAction(action);
       },
         
-      // Fetch the accounts from the Apex controller
+      // Fetch the contacts from the Apex controller
       showContactList: function(component, AccountId) {
-       var cmpEvent = $A.get('e.c:ContactListEvent'); 
-
-        cmpEvent.setParams({"AccountId" : AccountId}); 
-        cmpEvent.fire();
+      	var cmpEvent = $A.get('e.c:ContactListEvent'); 
+      	cmpEvent.setParams({"AccountId" : AccountId}); 
+      	cmpEvent.fire();
       },
         
       showPopupHelper: function(component, componentId, className){
         var modal = component.find(componentId);
         $A.util.removeClass(modal, className + 'hide');
         $A.util.addClass(modal, className + 'open');
-    },
-        
-       hidePopupHelper: function(component, componentId, className){
-           console.log('here');
+      },
+      hidePopupHelper: function(component, componentId, className){
         var modal = component.find(componentId);
         $A.util.addClass(modal, className+'hide');
         $A.util.removeClass(modal, className+'open');
-    }
+      },
+      createContactForAccount : function(component) {
+        var newContact = component.get("v.newContact");
+        var accountId = component.get("v.accountId");
+        var action = component.get('c.getContacts');
+        	
+        // Set up the callback
+        var contactString = { 'FirstName':newContact.FirstName,
+                                 'LastName':newContact.LastName,
+                                 'Email':newContact.Email  ,
+                                  'Phone':newContact.Phone 
+      	}
+            
+        console.log( contactString )
+        var contactStringNew = JSON.stringify(contactString);
+        console.log( contactStringNew )
         
+        action.setParams({ accountId : accountId, contactString : contactStringNew });
         
+        action.setCallback(this, function(actionResult) {
+            if("true" == actionResult.getReturnValue()){
+                var newContact = component.set("v.newContact", "");
+               
+               }
+        });
         
-        
-    })
+        $A.enqueueAction(action);  
+      } 
+    
+ 
+    
+})
